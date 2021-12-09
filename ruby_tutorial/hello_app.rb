4be5f3cde329c1c788642b3973f6e97b7dd8becb
Mary Kasparian <kasparian.mary@gmail.com>
2021-12-09 11:39:13 -0600

    finish Time exercises

--- ruby_tutorial/hello_app.rb
+++ ruby_tutorial/hello_app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+
+# DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
 get '/' do
-  'hello world'
+  dayname = Date::DAYNAMES[Time.now.wday]
+  "Hello, world! Happy #{dayname}"
 end
---
+++ ruby_tutorial/moon_landing.rb
@@ -0,0 +1,8 @@
+moon_landing = Time.utc(1969, 7, 20, 20, 7, 40)
+my_birthday = Time.utc(1989, 1, 25, 3, 20, 40)
+
+p my_birthday - moon_landing
+
+
+
+# i was born 615885180.0 seconds after the first moon landing
